473dc54dea12cc2dd7066b18e34357f34760b1cc
JCarlosSousa <123105914+JCarlosSousa@users.noreply.github.com>
2023-02-02 15:42:11 -0300

    Create Exc Matriz & Vetores Q3.c

---
+++ Exc Matriz & Vetores Q3.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//3. Fa¸ca um programa que recebe nomes de frutas e seu pre¸co. A quantidade frutas deve ser informada
+pelo o usuário. Utilize alocação dinâmica, especialmente a função realloc para não desperdiçar
+memória.
+  
+int main(void){
+	int quantidade;
+	printf("Digite a quantidade de frutas: ");
+	scanf("%d", &quantidade);
+    char **frutas = (char**) malloc(quantidade*sizeof(char*));
+    if(frutas == NULL){
+    	exit(1);
+	}
+	
+	int linha, coluna;
+	for(linha = 0; linha < quantidade; linha++){
+		frutas[linha] = (char*) malloc(50*sizeof(char));
+	}
+	
+	float *preco = (float*) malloc(quantidade*sizeof(float));
+	if (preco == NULL){
+		exit(1);
+	}
+	
+	for(linha = 0; linha < quantidade; linha++){
+		fflush(stdin);
+		printf("Informe o nome da fruta: \n");
+		scanf("%[^\n]s", frutas[linha]);
+		fflush(stdin);
+		printf("Informe o valor da fruta: \n");
+		scanf("%f", &preco[linha]);	
+	}
+	
+	for(linha = 0; linha < quantidade; linha++){
+		free(frutas[linha]);
+	}
+	
+	free(frutas);
+	free(preco);
+	return 0;
+}
