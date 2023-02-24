632a45bd1ed540162dbc4c62131558e725053fb1
JCarlosSousa <123105914+JCarlosSousa@users.noreply.github.com>
2023-02-24 14:42:01 -0300

    Create Exercicio Q2.c

---
+++ Vetores & Matrizes/Exercicio Q2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//2. Aloque dinamicamente uma matriz de inteiros 3x3 e preencha cada elemento da matriz com o seu
+//respectivo índice (matriz[i][j] = i*3 + j). Em seguida, imprima a matriz.
+
+int main(void){
+    int linhas = 3, colunas = 3;
+    int **matriz = (int**) malloc(linhas*sizeof(int*));
+    if(matriz == NULL){
+    	printf("Erro! Sem memória");
+    	exit(1);
+	}
+	
+    int linha;
+    for(linha =0; linha < linhas; linha++){
+    	matriz[linha] = (int*) malloc(colunas*sizeof(int));
+	}
+	
+	int i, j;
+	for(i = 0; i < linhas; i++){
+		for(j = 0; j < colunas; j++){
+			matriz[i][j] = i * 3 + j;
+		}
+	}
+	
+	for(i = 0; i < linhas; i++){
+		for(j = 0; j < colunas; j++){
+			printf("\n Matriz[%d][%d] = %d\n", i, j, matriz[i][j]);
+		}
+	}
+	
+	for(linha = 0; linha < linhas; linha++){
+		free(matriz[linha]);
+	}
+	
+	free(matriz);
+	return 0;
+}
